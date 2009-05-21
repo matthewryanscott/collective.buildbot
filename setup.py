a91005001bd82e9a8842f3a55e0e5998ee799312
gawel <gawel@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-05-21 09:34:04 +0000

    switch to 0.3.5
    git-svn-id: http://svn.plone.org/svn/collective/collective.buildbot/trunk@86541 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.getcwd(), *rnames)).read()
 
-version = '0.3.4'
+version = '0.3.5'
 
 long_description = (
     '\n.. contents::\n\n' +
